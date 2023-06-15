82ff441baebeb5bd11a1965b7fc83e024557dc88
alexeflorin <goremic@yahoo.com>
2023-06-15 11:57:43 -0400

    code to export module

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const CLI = require("./lib/cli.js")
+/* Modulul ./lib/cli.js exporta o functie care este returnata de require.
+Am atribuit rezultatul functia 'require' constantei CLI.
+Aceasta constanta memoreaza o functie care atunci cand este apelata returneaza un obiect de tipul lib/cli.js.
+*/
 
 new CLI().start()
+
+const inquirer= require("inquirer");
+
+const fs=require ("fs");
+
+
+
+
+
+
+
